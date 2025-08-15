d4b2a408829671800506107c11ccaaecd5c1dbca
Briany4717 <brianyan4717@hotmail.com>
2025-08-15 16:16:28 -0600

    Check task button visual error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -30,9 +30,9 @@ const TaskListItem = ({ id, title, description, accent, isCompleted, onToggle, i
     if (!animation) return;
     setAnimation(false);
     timeoutRef.current = setTimeout(() => {
+      onToggle(id);
       setAnimation(true);
       timeoutRef.current = null;
-      onToggle(id);
     }, (isCompleted ? 1200 : 1200));
   }, [animation, id, onToggle, isCompleted]);
 
@@ -45,7 +45,7 @@ const TaskListItem = ({ id, title, description, accent, isCompleted, onToggle, i
   }, []);
   return (
     <div onClick={() => onSelect(id)}
-      className={`${animation ? 'shadow-md group' : '-mt-21 -z-10'} items-center w-full h-18 bg--gray-200 border border-gray-200 rounded-lg flex snap-start ${isCompleted ? '' : 'delay-800'} transition-all duration-300 cursor-pointer relative`}>
+      className={`${animation ? 'shadow-md group' : '-mt-21 -z-10'} items-center w-full min-h-18 max-h-18 border border-gray-300 rounded-lg flex snap-start ${isCompleted ? '' : 'delay-800'} transition-all duration-300 cursor-pointer relative`}>
       <span className={`absolute material-symbols-rounded -z-1 ${isSelected && animation ? '-translate-x-8' : ''} ease-[cubic-bezier(.68,-0.55,.27,1.55)] transition-all duration-200`}
         style={{ fontSize: '2.0rem', color: isSelected ? accent : 'transparent' }}>chevron_right</span>
       <div className='bg-[#F8F9FA] w-full h-full flex items-center rounded-4xl justify-between pr-6'>
@@ -60,7 +60,7 @@ const TaskListItem = ({ id, title, description, accent, isCompleted, onToggle, i
         <span
           onClick={handleToggle}
           className={`material-symbols-rounded items-center justify-center cursor-pointer ${animation ? 'hover:text-blue-400 hover:scale-110' : ''} 
-            transition-all duration-200 overflow-scroll text-3 ${isCompleted ? (animation ? 'text-green-500' : '') : ''}`}
+            transition-all duration-200 overflow-hidden text-3 ${isCompleted ? (animation ? 'text-green-500' : '') : ''}`}
           style={{ fontSize: '2.0rem', fontVariationSettings: "'FILL' 1" }}
         >
 
@@ -108,7 +108,7 @@ const TiledMenu = ({ children, tiles, selectedTile, onSelect }) => {
           </div>
         ))}
       </div>
-      <div className='flex flex-col h-140 overflow-visible overflow-y-scroll px-6 pt-3 gap-3 border-t-1 border-gray-300'>
+      <div className='flex flex-col h-160 overflow-y-scroll px-6 pt-3 gap-3 border-t-1 border-gray-300 snap-y scroll-smooth'>
         {children}
       </div>
     </div>
